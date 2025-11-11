ca9e3c68b357a5ae8fc30a7d95acc959d1710e50
uygarpolat <uygarpolat@gmail.com>
2025-11-11 14:48:16 +0200

    switched to .env logic

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db' # sqlite (the todosaspp.db file that you can find in the same directory is for sqlite)
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost:5432/TodoApplicationDatabase' # postgresql
-# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase' # msql
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///todosapp.db' # test sqlite (the todosaspp.db file that you can find in the same directory is for sqlite)
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost:5432/TodoApplicationDatabase' # test postgresql
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase' # test mysql
 
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
